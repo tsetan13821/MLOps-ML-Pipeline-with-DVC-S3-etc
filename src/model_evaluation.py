1f42b32738e0763a96389b57756bfec5f7ff79f2
tsetan13821 <tsetan13821@gmail.com>
2026-04-26 03:13:19 +0530

    experiment tracking using dvclive

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,6 +5,8 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
+import yaml
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -29,7 +31,22 @@ logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
 
-
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters loaded from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('Error parsing YAML file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading parameters: %s', e)
+        raise
 def load_model(file_path: str):
     """Load the trained model from a file."""
     try:
@@ -95,6 +112,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
+        params = load_params(params_path = './params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -102,6 +120,11 @@ def main():
         y_test = test_data.iloc[:, -1].values
 
         metrics = evaluate_model(clf, X_test, y_test)
+        with Live(save_dvc_exp= True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+            live.log_params(params)
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
         logger.error('Failed to complete the model evaluation process: %s', e)
